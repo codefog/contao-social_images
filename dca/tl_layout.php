3ab1b8220b41f90808c33ecdf376a88d1e5c5393
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2013-11-30 10:23:39 +0100

    Some minor updates

--- dca/tl_layout.php
+++ dca/tl_layout.php
@@ -3,10 +3,10 @@
 /**
  * social_images extension for Contao Open Source CMS
  *
- * Copyright (C) 2013 Codefog Ltd
+ * Copyright (C) 2013 Codefog
  *
  * @package social_images
- * @author  Codefog Ltd <http://codefog.pl>
+ * @author  Codefog <http://codefog.pl>
  * @author  Kamil Kuzminski <kamil.kuzminski@codefog.pl>
  * @license LGPL
  */
