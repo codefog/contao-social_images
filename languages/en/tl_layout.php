dad524b04eb2b247e8ac5352dc0e491cd8a58055
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-06-26 15:49:47 +0200

    Reworked the image gathering mechanism (see #1)

--- languages/en/tl_layout.php
+++ languages/en/tl_layout.php
@@ -15,4 +15,12 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_layout']['socialImages'] = array('Enable social images', 'Enable the social images that can be used on Facebook and Google+.');
+$GLOBALS['TL_LANG']['tl_layout']['socialImages']       = array('Enable social images', 'Enable the social images that can be used on Facebook and Google+.');
+$GLOBALS['TL_LANG']['tl_layout']['socialImages_limit'] = array('Number of images', 'Here you can limit a number of images to be used. Set 0 to use all images.');
+$GLOBALS['TL_LANG']['tl_layout']['socialImages_size']  = array('Minimum width/height in pixels', 'Here you can enter a minimum dimensions in pixels that image must have to be used.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_layout']['socialImages_legend'] = 'Social images';
--- languages/pl/tl_layout.php
+++ languages/pl/tl_layout.php
@@ -15,4 +15,12 @@
 /**
  * Fields
  */
-$GLOBALS['TL_LANG']['tl_layout']['socialImages'] = array('Obrazki społecznościowe', 'Udostępnij obrazki dla sieci społecznościowych typu Facebook i Google+.');
+$GLOBALS['TL_LANG']['tl_layout']['socialImages']       = array('Obrazki społecznościowe', 'Udostępnij obrazki dla sieci społecznościowych typu Facebook i Google+.');
+$GLOBALS['TL_LANG']['tl_layout']['socialImages_limit'] = array('Liczba obrazków', 'Tutaj możesz wprowadzić limit liczby obrazków, które będą użyte. Wprowadź 0, aby użyć wszystkich obrazków.');
+$GLOBALS['TL_LANG']['tl_layout']['socialImages_size']  = array('Minimalna szerokość/wysokość w pikselach', 'Tutaj możesz wprowadzić minimalne wymiary w pikselach, który musi mieć obrazek, aby został użyty.');
+
+
+/**
+ * Legends
+ */
+$GLOBALS['TL_LANG']['tl_layout']['socialImages_legend'] = 'Obrazki społecznościowe';
