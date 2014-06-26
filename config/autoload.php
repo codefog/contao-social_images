c936f4cae54badb4c8a32522889b5d936eb3652f
Kamil Kuzminski <kamil.kuzminski@gmail.com>
2014-06-26 15:49:47 +0200

    Reworked the image gathering mechanism (see #1)

--- config/autoload.php
+++ config/autoload.php
@@ -23,6 +23,5 @@ ClassLoader::addNamespace('SocialImages');
  */
 ClassLoader::addClasses(array
 (
-	'SocialImages\SocialImages' => 'system/modules/social_images/classes/SocialImages.php',
-	'SocialImages\Image'        => 'system/modules/social_images/classes/Image.php'
+	'SocialImages\SocialImages' => 'system/modules/social_images/classes/SocialImages.php'
 ));
