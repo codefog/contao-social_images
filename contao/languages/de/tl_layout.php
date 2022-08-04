2ee0328cc9b3e096cb5c4c90d3d042e063a6803f
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2022-08-04 10:28:13 +0200

    Remove Google+ references

--- contao/languages/de/tl_layout.php
+++ contao/languages/de/tl_layout.php
@@ -1,4 +1,4 @@
 <?php
 
 // Fields
-$GLOBALS['TL_LANG']['tl_layout']['socialImages'] = ['Social Images aktivieren', 'Aktiviert die Social Images, welche auf Facebook und Google+ genutzt werden können.'];
+$GLOBALS['TL_LANG']['tl_layout']['socialImages'] = ['Social Images aktivieren', 'Aktiviert die Social Images, welche auf Facebook genutzt werden können.'];
--- contao/languages/pl/tl_layout.php
+++ contao/languages/pl/tl_layout.php
@@ -1,7 +1,7 @@
 <?php
 
 // Fields
-$GLOBALS['TL_LANG']['tl_layout']['socialImages'] = ['Obrazki społecznościowe', 'Udostępnij obrazki dla sieci społecznościowych typu Facebook i Google+.'];
+$GLOBALS['TL_LANG']['tl_layout']['socialImages'] = ['Obrazki społecznościowe', 'Udostępnij obrazki dla sieci społecznościowych typu Facebook.'];
 $GLOBALS['TL_LANG']['tl_layout']['socialImages_limit'] = ['Liczba obrazków', 'Tutaj możesz wprowadzić limit liczby obrazków, które będą użyte. Wprowadź 0, aby użyć wszystkich obrazków.'];
 $GLOBALS['TL_LANG']['tl_layout']['socialImages_size'] = ['Minimalna szerokość/wysokość w pikselach', 'Tutaj możesz wprowadzić minimalne wymiary w pikselach, który musi mieć obrazek, aby został użyty.'];
 
